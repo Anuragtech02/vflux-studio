8e5d7ce2366bc09c8b73236fba472d454db4ac61
Anuragtech02 <apal895@gmail.com>
2020-10-16 10:37:04 +0530

    Navbar styling done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./Navbar.module.css";
+import logo from "../../assets/logo/vflux-logo.png";
+import classNames from "classnames";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className={styles.container}>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <ul className={classNames(styles.navLinks, styles.flexRow)}>
+        <li className={styles.navLink}>Home</li>
+        <li className={styles.navLink}>About Us</li>
+        <li className={styles.navLink}>Our Services</li>
+        <li className={styles.navLink}>Contact Us</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
