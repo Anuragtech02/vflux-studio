8d1ce591d409575e1f41c6f7cc7c2850c3d1792c
Anuragtech02 <apal895@gmail.com>
2020-10-16 22:40:26 +0530

    Added lazy, suspense

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar, Footer, Home, Contact, About, Services } from "./components";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { Navbar, Footer } from "./components";
+
+const Home = lazy(() => import("./components/Home/Home"));
+const About = lazy(() => import("./components/About/About"));
+const Services = lazy(() => import("./components/Services/Services"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
 
 const App = () => {
   return (
     <Router>
-      <div className={styles.nav}>
-        <Navbar />
-      </div>
-      <div className={styles.page}>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/our-services" exact component={Services} />
-        </Switch>
-      </div>
-      <footer className={styles.footer}>
-        <Footer />
-      </footer>
+      <Suspense fallback={Loading}>
+        <div className={styles.nav}>
+          <Navbar />
+        </div>
+        <div className={styles.page}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/our-services" exact component={Services} />
+          </Switch>
+        </div>
+        <footer className={styles.footer}>
+          <Footer />
+        </footer>
+      </Suspense>
     </Router>
   );
 };
 
 export default App;
+
+const Loading = () => {
+  return (
+    <div className={styles.loading}>
+      <CircularProgress />
+    </div>
+  );
+};
