4f4c70c99d6f887a3caf08aa800ac93d615bb620
Anuragtech02 <apal895@gmail.com>
2020-10-16 11:13:24 +0530

    Footer styling done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./Footer.module.css";
+import classNames from "classnames";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={classNames(styles.social, styles.flexRow)}>
+        <i className="fab fa-facebook-square"></i>
+        <i className="fab fa-behance"></i>
+        <i className="fab fa-youtube"></i>
+        <i className="fab fa-instagram"></i>
+      </div>
+      <div className={styles.copyright}>
+        <p>© 2020 Unified Consulting. All Right Reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
