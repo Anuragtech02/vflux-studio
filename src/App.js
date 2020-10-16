d86231db36553c8779e76e7af42c90d7a92a38c6
Anuragtech02 <apal895@gmail.com>
2020-10-16 10:16:43 +0530

    Created common css and app scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styles from "./App.module.css";
+import { Navbar, Footer, Home } from "./components";
 
 const App = () => {
-    return (
-        <div>
+  return (
+    <div className={styles.container}>
+      <Router>
+        <div className={styles.nav}>
+          <Navbar />
         </div>
-    )
-}
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <div className={styles.footer}>
+          <Footer />
+        </div>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
